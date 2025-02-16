f1489406c057b34bcacf1ce954e2c5c7e2a8f0d1
Thomas Pockrandt <me@thomaspockrandt.com>
2025-02-16 16:37:08 +0100

    Add wrangler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  ssr: true
+  ssr: true,
+  nitro: {
+    preset: 'static',
+    devProxy: {
+      '/api/': 'http://localhost:8788/api/',
+    }
+  }
 })
